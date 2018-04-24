f188728619b99fcf94e0aff8712b98a4fd4c7a1f
WolverinDEV <git@mcgalaxy.de>
2018-04-24 18:10:15 +0200

    Transformed project into CMake  and removed dll's

--- src/sha512.h
+++ src/sha512.h
@@ -13,7 +13,6 @@ typedef struct sha512_context_ {
 } sha512_context;
 
 
-int sha512_init(sha512_context * md);
 int sha512_final(sha512_context * md, unsigned char *out);
 int sha512_update(sha512_context * md, const unsigned char *in, size_t inlen);
 int sha512(const unsigned char *message, size_t message_len, unsigned char *out);
